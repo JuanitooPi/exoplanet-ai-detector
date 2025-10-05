9125566b470e22d028fe8b4ee9992c0913f58311
JuanitooPi <andresJuansanchezcusneros8@gmail.com>
2025-10-05 12:48:14 -0500

    Create setup.sh

---
+++ exoplanet-ai-detector/setup.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# setup.sh - Script de instalación forzada
+pip install --upgrade pip
+pip install streamlit==1.28.0
+pip install pandas==1.5.3
+pip install numpy==1.24.3
+pip install scikit-learn==1.2.2
+pip install xgboost==1.7.5
+pip install lightgbm==4.1.0
+pip install plotly==5.15.0
+pip install joblib==1.2.0
